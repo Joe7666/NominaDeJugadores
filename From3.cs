dc126cdbe250bc3a0a4a5e3ab2dc20b14aac0622
Joe7666 <joecalderon935@gmail.com>
2022-03-13 17:11:50 -0500

    Se añadio un metodo para ver leer una cadena

---
+++ From3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NominaDeJugadores
+{
+    public partial class From3 : Form
+    {
+        public Lista lista3 = new Lista();
+        public From3()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVer_Click(object sender, EventArgs e)
+        {
+            dgvPosicion.DataSource = lista3.CadenaPo(mtxtPosicion.Text);
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            mtxtPosicion.Text = " ";
+        }
+    }
+}
--- Lista.cs
+++ Lista.cs
@@ -18,5 +18,17 @@ namespace NominaDeJugadores
         {
             Jugadores.Add(Jugador);
         }
+        public List<Jugador> CadenaPo(string cadena)
+        {
+            List<Jugador> aux = new List<Jugador>();
+            foreach(Jugador jugador in Jugadores)
+            {
+                if(jugador.Posicion.Contains(cadena))
+                {
+                    aux.Add(jugador);
+                }
+            }
+            return aux;
+        }
     }
 }
